c4c6402cd378008711d92b9ec3851e0fa02e2a23
Cardiox12 <cardiox1212@hotmail.fr>
2019-02-16 22:40:13 +0100

    finished implementing LBCArticle

---
+++ LBCScrapper/LBCArticle.py
@@ -0,0 +1,6 @@
+class LBCArticle(object):
+    def __init__(self, title, price, location, link):
+        self.title = title
+        self.price = price
+        self.location = location
+        self.link = link
